e27755a854cc94e1d3c75b7dce7869fde576bf52
HareemA <ahareem134@gmail.com>
2024-06-08 21:50:48 +0500

    Trying to make build work

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -82,7 +82,18 @@ export default function LoginScreen() {
       // navigation.navigate('ChildRegistration', { parentId })
       navigation.navigate('ChildProfile' , {parentId})
     } catch (error) {
-      console.error("Login failed:", error.message);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an error
+        console.log('Error', error.message);
+      }
       Alert.alert("Login Failed", "Login Unsuccessful");
     }
   };
